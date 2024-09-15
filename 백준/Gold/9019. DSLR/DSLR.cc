375282ef954444ba3a8b3b8ebb470dc6176b61c1
JeongHyun Han <81086444+jhgl0419@users.noreply.github.com>
2024-09-15 14:06:25 +0900

    [Gold IV] Title: DSLR, Time: 2692 ms, Memory: 2196 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -4,22 +4,22 @@
 
 using namespace std;
 
-int D(int n) {
+inline int D(int n) {
 	return 2 * n % 10'000;
 }
 
-int S(int n) {
+inline int S(int n) {
 	return n == 0 ? 9'999 : n - 1;
 }
 
-int L(int n) {
+inline int L(int n) {
 	int d1 = n / 1000;
 	int other = (n % 1000) * 10;
 
 	return d1 + other;
 }
 
-int R(int n) {
+inline int R(int n) {
 	int d4 = (n % 10) * 1000;
 	int other = n / 10;
 
@@ -33,7 +33,7 @@ int main() {
 	int T;
 	scanf("%d", &T);
 
-	int A, B;
+	int A, B, d, s, l, r;
 
 	for (int test_case = 1; test_case <= T; test_case++) {
 		scanf("%d %d", &A, &B);
@@ -43,16 +43,32 @@ int main() {
 
 		while (!q.empty()) {
 			auto t = q.front(); q.pop();
-			if (t.first == B) {
+		
+            if (t.first == B) {
 				cout << t.second << "\n";
 				break;
 			}
-			if (visit[t.first]) continue;
-			visit[t.first] = 1;
-			q.push({ D(t.first), t.second + "D" });
-			q.push({ S(t.first), t.second + "S" });
-			q.push({ L(t.first), t.second + "L" });
-			q.push({ R(t.first), t.second + "R" });
+
+			d = D(t.first);
+            s = S(t.first);
+            l = L(t.first);
+            r = R(t.first);
+            if(!visit[d]) {
+                q.push({ d, t.second + "D" });
+                visit[d] = 1;
+            }
+            if(!visit[s]) {
+                q.push({ s, t.second + "S" });
+                visit[s] = 1;
+            }
+            if(!visit[l]) {
+                q.push({ l, t.second + "L" });
+                visit[l] = 1;
+            }
+            if(!visit[r]) {
+                q.push({ r, t.second + "R" });
+                visit[r] = 1;
+            }
 		}
 	}
 
